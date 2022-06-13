7e79f09c1833bb50ab546ea122dd96fa8a5e2164
rstanbaugh <rich_bootcamp@icloud.com>
2022-06-13 18:45:34 -0400

    added mysql connector

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,8 +8,25 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    // insert MySQL password
+    password: 'ill1nois',
+    database: 'election'
+  },
+  console.log('connected to the election database')
+);
 
 
+
+
+// test query
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 // routes
 app.get('/', (req, res) => {
