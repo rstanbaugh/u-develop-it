76b4ac200348507fed2b69a4bdcac8e853dfcdc9
rstanbaugh <rich_bootcamp@icloud.com>
2022-06-14 08:14:14 -0400

    db routes added: GET ALL, GET 1, DELETE, POST

---
+++ db/rs-script.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS election;
+
+CREATE DATABASE election;
+USE election;
+
+CREATE TABLE candidates (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  industry_connected BOOLEAN NOT NULL
+);
+
+INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES
+  ('Ronald', 'Firbank', 1),
+  ('Virginia', 'Woolf', 1),
+  ('Piers', 'Gaveston', 0),
+  ('Charles', 'LeRoi', 1),
+  ('Katherine', 'Mansfield', 1),
+  ('Dora', 'Carrington', 0),
+  ('Edward', 'Bellamy', 0),
+  ('Montague', 'Summers', 1),
+  ('Octavia', 'Butler', 1),
+  ('Unica', 'Zurn', 1);
+
