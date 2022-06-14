0b9bc3a88a28aa8f447e6be0c1687440cafbe0e8
rstanbaugh <rich_bootcamp@icloud.com>
2022-06-14 12:30:16 -0400

    updated queries with LEFT JOIN

--- db/rs-script.sql
+++ db/rs-script.sql
@@ -1,32 +1,8 @@
 DROP DATABASE IF EXISTS election;
 
-CREATE DATABASE election;
-USE election;
+SOURCE ./db.db.sql;
 
-CREATE TABLE candidates (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
-);
+SOURCE ./db.schema.sql;
 
-CREATE TABLE parties (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
-);
-
-INSERT INTO candidates (first_name, last_name, industry_connected)
-VALUES
-  ('Ronald', 'Firbank', 1),
-  ('Virginia', 'Woolf', 1),
-  ('Piers', 'Gaveston', 0),
-  ('Charles', 'LeRoi', 1),
-  ('Katherine', 'Mansfield', 1),
-  ('Dora', 'Carrington', 0),
-  ('Edward', 'Bellamy', 0),
-  ('Montague', 'Summers', 1),
-  ('Octavia', 'Butler', 1),
-  ('Unica', 'Zurn', 1);
+SOURCE ./db.seeds.sql;
 
